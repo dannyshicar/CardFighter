07184b5c7ab38694b610109904f7a57316da553b
danny0rn0t <b08902144@ntu.edu.tw>
2022-12-15 06:48:20 +0800

    skill selection menu v1

---
+++ Assets/Scripts/SkillManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillManager : MonoBehaviour
+{
+    public SkillDatabase skillDB;
+
+    [SerializeField] TMPro.TextMeshProUGUI nameText;
+    [SerializeField] Image DisplayImage;
+
+    const int MAX_SKILL_SELECT = 5;
+    int[] selectedSkills = new int[MAX_SKILL_SELECT];
+    [SerializeField] Image[] selectedSkillImage = new Image[MAX_SKILL_SELECT];
+    int currentSkillSelect = 0;
+    int currentDisplaySkill = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateSkill();
+    }
+
+    private bool IsSkillSelected(int skillID)
+    {
+        for (int i = 0; i < currentSkillSelect; i++)
+        {
+            if (selectedSkills[i] == skillID)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void NextOption()
+    {
+        do
+        {
+            currentDisplaySkill = (currentDisplaySkill + 1) % skillDB.SkillCount;
+        } while (IsSkillSelected(currentDisplaySkill));
+        UpdateSkill();
+    }
+    public void BackOption()
+    {
+        do
+        {
+            currentDisplaySkill = (currentDisplaySkill - 1) % skillDB.SkillCount;
+        } while (IsSkillSelected(currentDisplaySkill));
+        UpdateSkill();
+    }
+    public void SelectOption()
+    {
+        selectedSkills[currentSkillSelect] = currentDisplaySkill;
+        selectedSkillImage[currentSkillSelect].sprite = skillDB.GetSkill(currentDisplaySkill).skillSprite;
+        currentSkillSelect++;
+        currentDisplaySkill = skillDB.SkillCount - 1;
+        NextOption();
+        if (currentSkillSelect == MAX_SKILL_SELECT)
+        {
+            StartGame();
+        }
+    }
+
+    private void UpdateSkill()
+    {
+        SkillStat skillStat = skillDB.GetSkill(currentDisplaySkill);
+        nameText.text = skillStat.skillName;
+        DisplayImage.sprite = skillStat.skillSprite;
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Starting Game");
+    }
+
+}
---
+++ Assets/Scripts/SkillStat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkillStat
+{
+    public string skillName;
+    public Sprite skillSprite;
+}
